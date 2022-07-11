844021da512b4eba29283bb17441ff079c98f0b8
Mateusz <matesz1kb@gmail.com>
2022-07-11 02:53:50 +0200

    Extended User model

--- models/user.go
+++ models/user.go
@@ -6,7 +6,23 @@ import (
 
 type User struct {
 	gorm.Model
-	Login    string `json:"login"`
-	Email    string `json:"email"`
-	Password string `json:"password"`
+	Login       string    `json:"login"`
+	Email       string    `json:"email"`
+	Password    string    `json:"password"`
+	ItemsInCart []Product `json:"itemsInCart" gorm:"many2many:user_carts;"`
+	Orders      []Order   `json:"orders"`
+	Addresses   []Address `json:"addresses"`
+}
+
+type Address struct {
+	gorm.Model
+	UserID      uint
+	FirstName   string `json:"firstName"`
+	LastName    string `json:"lastName"`
+	Street      string `json:"street"`
+	HouseNumber string `json:"houseNumber"`
+	PostalCode  string `json:"postalode"`
+	City        string `json:"city"`
+	Country     string `json:"country"`
+	PhoneNumber string `json:"phoneNumber"`
 }

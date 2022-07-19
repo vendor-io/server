2c3277598cbcd4c45aee5d87d2c9975f3308ec01
Mateusz <matesz1kb@gmail.com>
2022-07-19 22:41:30 +0200

    Updated routes

--- route/route.go
+++ route/route.go
@@ -14,4 +14,10 @@ func Init(g *echo.Group) {
 	g.GET("/users", controllers.GetAllUsers)
 	g.GET("/users/:id", controllers.GetUserById)
 	g.POST("/users", controllers.CreateUser)
+
+	g.GET("/products", controllers.GetAllProducts)
+	g.POST("/products", controllers.AddNewProduct)
+
+	g.GET("/categories", controllers.GetAllCategories)
+	g.POST("/categories", controllers.AddNewCategory)
 }

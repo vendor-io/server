6bdfbe1b3c6a1b274ed59faf852ad3a3887aac13
Mateusz <matesz1kb@gmail.com>
2022-08-01 22:58:05 +0200

    Added NameConverter

--- util/name_converter.go
+++ util/name_converter.go
@@ -7,9 +7,11 @@ import (
 
 var matchFirstCap = regexp.MustCompile("(.)([A-Z][a-z]+)")
 var matchAllCap = regexp.MustCompile("([a-z0-9])([A-Z])")
+var matchAllSpaces = regexp.MustCompile(`\s`)
 
 func ToSnakeCase(str string) string {
 	snake := matchFirstCap.ReplaceAllString(str, "${1}_${2}")
 	snake = matchAllCap.ReplaceAllString(snake, "${1}_${2}")
+	snake = matchAllSpaces.ReplaceAllString(snake, "${1}${2}")
 	return strings.ToLower(snake)
 }

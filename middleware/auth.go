fbf0a8338566d9754bd9806b354dea8abaf06199
Mateusz <matesz1kb@gmail.com>
2022-08-03 01:04:25 +0200

    Added Context handler with token

---
+++ middleware/auth.go
@@ -0,0 +1,37 @@
+package middleware
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"strings"
+
+	"firebase.google.com/go/auth"
+	"github.com/labstack/echo/v4"
+)
+
+func Auth(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		firebaseAuth := c.Get("firebaseAuth").(*auth.Client)
+
+		authorizationToken := c.Request().Header.Get("Authorization")
+		fmt.Println(authorizationToken)
+		idToken := strings.TrimSpace(strings.Replace(authorizationToken, "Bearer", "", 1))
+		fmt.Println(idToken)
+
+		if idToken == "" {
+			c.String(http.StatusBadRequest, "Id token not available")
+			panic("Id token missing")
+		}
+
+		token, err := firebaseAuth.VerifyIDToken(context.Background(), idToken)
+
+		if err != nil {
+			c.String(http.StatusBadRequest, "Invalid Token")
+			return err
+		}
+
+		c.Set("UUID", token.UID)
+		return next(c)
+	}
+}

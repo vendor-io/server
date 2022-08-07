3149c38e52ef28c2da7c13d88d711fa550f68b99
Mateusz <matesz1kb@gmail.com>
2022-08-07 04:43:15 +0200

    Moved Address model to separate module

---
+++ models/dto/address.go
@@ -0,0 +1,13 @@
+package dto
+
+type AddressDTO struct {
+	UserID      string `json:"userId"`
+	FirstName   string `json:"firstName"`
+	LastName    string `json:"lastName"`
+	Street      string `json:"street"`
+	HouseNumber string `json:"houseNumber"`
+	PostalCode  string `json:"postalCode"`
+	City        string `json:"city"`
+	Country     string `json:"country"`
+	PhoneNumber string `json:"phoneNumber"`
+}
--- models/user.go
+++ models/user.go
@@ -9,16 +9,3 @@ type User struct {
 	Orders      []Order   `json:"orders"`
 	Addresses   []Address `json:"addresses"`
 }
-
-type Address struct {
-	GormModel
-	UserID      uint   `json:"userId"`
-	FirstName   string `json:"firstName"`
-	LastName    string `json:"lastName"`
-	Street      string `json:"street"`
-	HouseNumber string `json:"houseNumber"`
-	PostalCode  string `json:"postalode"`
-	City        string `json:"city"`
-	Country     string `json:"country"`
-	PhoneNumber string `json:"phoneNumber"`
-}

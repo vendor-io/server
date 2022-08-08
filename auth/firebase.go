593b482396304a39b21299f021df111459df0d4b
Mateusz <matesz1kb@gmail.com>
2022-08-08 20:45:43 +0200

    Added Firebase token creation from .env

--- auth/firebase.go
+++ auth/firebase.go
@@ -2,6 +2,8 @@ package auth
 
 import (
 	"context"
+	"encoding/json"
+	"io/ioutil"
 	"os"
 	"path/filepath"
 
@@ -11,8 +13,41 @@ import (
 	"google.golang.org/api/option"
 )
 
+type FirebaseConfig struct {
+	Type                    string `json:"type"`
+	ProjectID               string `json:"project_id"`
+	PrivateKeyID            string `json:"private_key_id"`
+	PrivateKey              string `json:"private_key"`
+	ClientEmail             string `json:"client_email"`
+	ClientID                string `json:"client_id"`
+	AuthURI                 string `json:"auth_uri"`
+	TokenURI                string `json:"token_uri"`
+	AuthProviderX509CertURL string `json:"auth_provider_x509_cert_url"`
+	ClientX509CertURL       string `json:"client_x509_cert_url"`
+}
+
+func CreateFirebaseInitJson() {
+	config := FirebaseConfig{
+		Type:                    os.Getenv("FIREBASE_TYPE"),
+		ProjectID:               os.Getenv("FIREBASE_PROJECT_ID"),
+		PrivateKeyID:            os.Getenv("FIREBASE_PRIVATE_KEY_ID"),
+		PrivateKey:              os.Getenv("FIREBASE_PRIVATE_KEY"),
+		ClientEmail:             os.Getenv("FIREBASE_CLIENT_EMAIL"),
+		ClientID:                os.Getenv("FIREBASE_CLIENT_ID"),
+		AuthURI:                 os.Getenv("FIREBASE_AUTH_URI"),
+		TokenURI:                os.Getenv("FIREBASE_TOKEN_URI"),
+		AuthProviderX509CertURL: os.Getenv("FIREBASE_AUTH_PROVIDER_X509_CERT_URL"),
+		ClientX509CertURL:       os.Getenv("FIREBASE_CLIENT_X509_CERT_URL"),
+	}
+
+	file, _ := json.MarshalIndent(config, "", "  ")
+
+	_ = ioutil.WriteFile(os.Getenv("FIREBASE_PRIVATE_KEY_JSON"), file, 0644)
+}
+
 func InitFirebase() *auth.Client {
-	serviceAccountKeyFilePath, err := filepath.Abs(os.Getenv("FIREBASE_PRIVATE_KEY"))
+	serviceAccountKeyFilePath, err := filepath.Abs(os.Getenv("FIREBASE_PRIVATE_KEY_JSON"))
+
 	if err != nil {
 		panic("Unable to load serviceAccountKeys.json file")
 	}
@@ -21,12 +56,12 @@ func InitFirebase() *auth.Client {
 
 	app, err := firebase.NewApp(context.Background(), nil, opt)
 	if err != nil {
-		panic("Firebase load error")
+		panic("Firebase load error: Base app")
 	}
 
 	auth, err := app.Auth(context.Background())
 	if err != nil {
-		panic("Fireauth load error")
+		panic("Firebase load error: Auth")
 	}
 	return auth
 }

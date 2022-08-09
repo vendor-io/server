a07fead67e24930dd24e8279a0e21c8bdd4dfa78
Mateusz <matesz1kb@gmail.com>
2022-08-09 02:56:55 +0200

    Removed unused controller

--- route/user.go
+++ route/user.go
@@ -1,19 +1,12 @@
 package route
 
 import (
-	"fmt"
 	"keyboardify-server/controllers"
-	"net/http"
-	"os"
 
 	"github.com/labstack/echo/v4"
 )
 
 func InitUser(g *echo.Group) {
-	g.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, fmt.Sprintf("%s Backend 0.0.1", os.Getenv("APP")))
-	})
-
 	g.GET("/su/:uid", controllers.CheckIfUserIsSuperUser)
 
 	g.GET("/products", controllers.GetAllProducts)
